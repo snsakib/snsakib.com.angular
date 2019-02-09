d3a223722e788c5ced359ff12658f68cd9a756dd
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-02-09 09:36:45 +0600

    lazy load all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,9 +18,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules
-    })
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
