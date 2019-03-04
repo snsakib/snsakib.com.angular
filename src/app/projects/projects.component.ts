f68c2368308a4ead1fba5b23f81d1eb6befc08b4
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-03-04 20:44:18 +0600

    update projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,15 +9,54 @@ export class ProjectsComponent {
   //list of projects
   projects = [
     {
-      projectTitle: 'Uni',
-      description: 'Website Template',
+      title: 'Project Title 1',
       category: 'Website',
       tags: ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'Angular'],
-      liveDemoLink: 'https://snsakib.github.io/uni/',
-      imagePath: 'assets/images/projects/uni.jpg',
-      alt: 'Screenshot of the project'
+      liveDemoLink: '#',
+      imgPath: 'http://via.placeholder.com/640x360',
+      imgAlt: 'Screenshot of the project'
+    },
+    {
+      title: 'project Title 2',
+      category: 'Website',
+      tags: ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'Angular'],
+      liveDemoLink: '#',
+      imgPath: 'http://via.placeholder.com/640x460',
+      imgAlt: 'Screenshot of the project'
+    },
+    {
+      title: 'Project Title 3',
+      category: 'Website',
+      tags: ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'Angular'],
+      liveDemoLink: '#',
+      imgPath: 'http://via.placeholder.com/640x560',
+      imgAlt: 'Screenshot of the project'
     }
   ];
+  slideIndex = 1;
+  slides = document.getElementsByClassName('mySlides');
+  dots = document.getElementsByClassName('dot');
+
+  showSlides(n) {
+    if (n > this.slides.length) {
+      this.slideIndex = 1;
+    }
+    if (n < 1) {
+      this.slideIndex = this.slides.length;
+    }
+    for (let i = 0; i < this.slides.length; i++) {
+      (<HTMLElement>this.slides[i]).style.display = 'none';
+    }
+    (<HTMLElement>this.slides[this.slideIndex - 1]).style.display = 'block';
+  }
+
+  plusSlides(n) {
+    this.showSlides((this.slideIndex += n));
+  }
+
+  ngAfterViewInit() {
+    this.showSlides(this.slideIndex);
+  }
 
   constructor() {}
 }
