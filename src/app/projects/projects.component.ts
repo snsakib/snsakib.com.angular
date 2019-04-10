73f97733f5231a4aeadc2c578af3333cba433e19
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-04-10 06:11:55 +0600

    Update about page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,10 +9,11 @@ export class ProjectsComponent {
   //list of projects
   projects = [
     {
-      title: 'Project Title 1',
+      title: 'Agency Website',
       category: 'Website',
       tags: ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'Angular'],
-      liveDemoLink: '#',
+      liveDemo: '#',
+      caseStudy: '#',
       imgPath: 'http://placeimg.com/560/320/nature/grayscale',
       imgAlt: 'Screenshot of the project'
     },
@@ -20,7 +21,8 @@ export class ProjectsComponent {
       title: 'project Title 2',
       category: 'Website',
       tags: ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'Angular'],
-      liveDemoLink: '#',
+      liveDemo: 'https://www.snsakib.com/uni',
+      caseStudy: '#',
       imgPath: 'http://placeimg.com/560/320/arch/grayscale',
       imgAlt: 'Screenshot of the project'
     },
@@ -28,7 +30,8 @@ export class ProjectsComponent {
       title: 'Project Title 3',
       category: 'Website',
       tags: ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'Angular'],
-      liveDemoLink: '#',
+      liveDemo: '#',
+      caseStudy: '#',
       imgPath: 'http://placeimg.com/560/320/tech/grayscale',
       imgAlt: 'Screenshot of the project'
     }
