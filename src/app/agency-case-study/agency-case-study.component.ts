17e43e40fad0b544d9d3044d9768cf985edf2547
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-04-24 16:13:16 +0600

    feat: create agecy-case-study component & module

---
+++ src/app/agency-case-study/agency-case-study.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'agency-case-study',
+  templateUrl: './agency-case-study.component.html',
+  styleUrls: ['./agency-case-study.component.scss']
+})
+export class AgencyCaseStudyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/agency-case-study/agency-case-study.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgencyCaseStudyComponent } from './agency-case-study.component';
+
+@NgModule({
+  declarations: [AgencyCaseStudyComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AgencyCaseStudyModule { }
