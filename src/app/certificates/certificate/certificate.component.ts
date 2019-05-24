bd4e5b663c001cb4fe7c1ffd682267151f28e02f
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-05-25 05:29:32 +0600

    feat: create certificate component

---
+++ src/app/certificates/certificate/certificate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'certificate',
+  templateUrl: './certificate.component.html',
+  styleUrls: ['./certificate.component.scss']
+})
+export class CertificateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
