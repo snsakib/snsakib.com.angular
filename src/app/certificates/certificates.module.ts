61b75db5c4f933017b3a67f3aa8c48aab08de550
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-05-25 06:02:02 +0600

    feat: import PdfViewerModule into certificates module

--- src/app/certificates/certificates.module.ts
+++ src/app/certificates/certificates.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CertificatesRoutingModule } from './certificates-routing.module';
 import { CertificateComponent } from './certificate/certificate.component';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @NgModule({
   declarations: [CertificateComponent],
-  imports: [
-    CommonModule,
-    CertificatesRoutingModule
-  ]
+  imports: [CommonModule, CertificatesRoutingModule, PdfViewerModule]
 })
-export class CertificatesModule { }
+export class CertificatesModule {}
