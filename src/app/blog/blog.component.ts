3d5b6436cc129c04ac9cb7050b5030cfae0d6290
Syed Nazmus Sakib <contact@snsakib.com>
2019-07-05 22:13:40 +0600

    refactor: refactor blog component for showing blog category

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,47 +1,12 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "blog",
-  templateUrl: "./blog.component.html",
-  styleUrls: ["./blog.component.scss"]
+  selector: 'blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent {
-  //TODO
-  //Move this data to 
-  postFilterOptions = ['Angular', 'JavaScript', 'TypeScript'];
-
-  posts = [
-    {
-      title: "Angular Reactive Form",
-      date: "11 Nov, 2018",
-      timeToRead: "5",
-      routerLink: "/projects"
-    },
-    {
-      title: "Project 2",
-      date: "11 Nov, 2018",
-      timeToRead: "5",
-      routerLink: "/projects"
-    },
-    {
-      title: "Project 3",
-      date: "11 Nov, 2018",
-      timeToRead: "5",
-      routerLink: "/projects"
-    },
-    {
-      title: "Project 4",
-      date: "11 Nov, 2018",
-      timeToRead: "5",
-      routerLink: "/projects"
-    },
-    {
-      title: "Project 5",
-      date: "11 Nov, 2018",
-      timeToRead: "5",
-      routerLink: "/projects"
-    }
-  ];
+  topics: String[] = [];
 
   constructor() {}
 }
