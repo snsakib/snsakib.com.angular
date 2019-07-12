a72b38c28551e12314cc1662a00ac5002bc63bb0
Syed Nazmus Sakib <contact@snsakib.com>
2019-07-12 10:46:37 +0600

    feat: add logic to get data from server in blog.component

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'blog',
-  templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  selector: "blog",
+  templateUrl: "./blog.component.html",
+  styleUrls: ["./blog.component.scss"]
 })
-export class BlogComponent {
-  topics: String[] = [];
+export class BlogComponent implements OnInit {
+  topics: Object = {};
+
+  getPostsCategories() {
+    return this.http.get(
+      "https://snsakib-blog-cms.herokuapp.com/api/postsCategories"
+    );
+  }
 
   constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getPostsCategories().subscribe(res => {
+      this.topics = res;
+    });
+  }
 }
