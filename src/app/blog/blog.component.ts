94993db627cc52236988012fedae9e861c355f24
Syed Nazmus Sakib <contact@snsakib.com>
2019-07-12 10:01:02 +0600

    feat: inject HttpClient module in blog.componet

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'blog',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
 export class BlogComponent {
   topics: String[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 }
