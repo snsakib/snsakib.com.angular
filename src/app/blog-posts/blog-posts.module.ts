b0017e2349e608ca03e1845c59561ed473bf1579
Syed Nazmus Sakib <contact@snsakib.com>
2019-07-15 13:00:43 +0600

    feat: import HttpClientModule in blog-posts.module.ts

--- src/app/blog-posts/blog-posts.module.ts
+++ src/app/blog-posts/blog-posts.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogPostsComponent } from './blog-posts.component';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [BlogPostsComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     RouterModule.forChild([
       {
         path: '',
