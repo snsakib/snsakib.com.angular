6f9c246f160620bf94f085e01db27a50e402ff96
Syed Nazmus Sakib <contact@snsakib.com>
2019-07-15 10:48:11 +0600

    feat: add routing in blog-posts.module

--- src/app/blog-posts/blog-posts.module.ts
+++ src/app/blog-posts/blog-posts.module.ts
@@ -1,9 +1,19 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { BlogPostsComponent } from "./blog-posts.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogPostsComponent } from './blog-posts.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [BlogPostsComponent],
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: BlogPostsComponent,
+        data: {animation: 'blogPostsComponent'}
+      }
+    ])
+  ]
 })
-export class BlogPostsModule {}
+export class BlogPostsModule { }
