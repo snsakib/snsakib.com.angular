05f99671311d6fd1346f2fc32515aaf5eb0af8dd
Syed Nazmus Sakib <contact@snsakib.com>
2019-09-18 19:26:16 +0600

    change live demo url

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -14,7 +14,7 @@ export class ProjectsComponent {
       description:
         'A responsive webpage developed as a personal project of mine to refurbish my front-end development skills.',
       tags: ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'Angular'],
-      liveDemo: '/projects/agency/live-demo',
+      liveDemo: 'https://project-agency.netlify.com',
       caseStudy: '/projects/agency/case-study',
       imgPath: 'assets/images/projects/agency/agency-screenshot-280.jpg',
       imgAlt: 'Screenshot of the project'
