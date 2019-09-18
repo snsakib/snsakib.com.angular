78c0512a55ed2afe434f669734149f1b815c0d04
Syed Nazmus Sakib <nazmus.sakib678@gmail.com>
2019-09-18 17:27:20 +0600

    Update projects.component.ts

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -14,7 +14,7 @@ export class ProjectsComponent {
       description:
         'A responsive webpage developed as a personal project of mine to refurbish my front-end development skills.',
       tags: ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'Angular'],
-      liveDemo: 'https://project-agency.netlify.com/',
+      liveDemo: '/projects/agency/live-demo',
       caseStudy: '/projects/agency/case-study',
       imgPath: 'assets/images/projects/agency/agency-screenshot-280.jpg',
       imgAlt: 'Screenshot of the project'
