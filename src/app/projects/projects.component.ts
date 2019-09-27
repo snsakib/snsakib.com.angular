e0e6fdc1caf8111d559fa41f240f64150d45676b
Syed Nazmus Sakib <contact@snsakib.com>
2019-09-27 15:23:42 +0600

    feat: change _redirects rule for case-study

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -15,7 +15,7 @@ export class ProjectsComponent {
         'A responsive webpage developed as a personal project of mine to refurbish my front-end development skills.',
       tags: ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'Angular'],
       liveDemo: 'https://project-agency.netlify.com',
-      caseStudy: '/projects/agency/case-study',
+      caseStudy: '/case-study/agency',
       imgPath: 'assets/images/projects/agency/agency-screenshot-280.jpg',
       imgAlt: 'Screenshot of the project'
     }
