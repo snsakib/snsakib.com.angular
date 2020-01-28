5213dc5c54c1e9814634f5fcbf76b15d0b4620ff
snsakib <contact@snsakib.com>
2020-01-29 02:32:44 +0600

    refactor projects module's routing

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,25 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-// Module for router configuration
-import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 
 // Components
 import { ProjectsComponent } from './projects.component';
+import { ProjectsRoutingModule } from './projects-routing.module';
 
 @NgModule({
   declarations: [ProjectsComponent],
   imports: [
     CommonModule,
     MatCardModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: ProjectsComponent,
-        data: { animation: 'projectsPage' }
-      }
-    ])
+    ProjectsRoutingModule
   ]
 })
 export class ProjectsModule {}
